83da82092ebddf6bda7ddbb17ca3f365c427ae77
Andrei Muntean <andreymuntean2004@gmail.com>
2022-06-24 21:40:00 +0300

    Added comments

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ import nodemailer from "nodemailer";
 import { db } from "./firebase.js";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 
+// Define the structure of the article
 const latestArticle = { title: "", src: "", url: "" };
 
 const scrape = async () => {
+  // Fetch the website content and parse it with cheerio
 	const url = "https://www.jw.org/ro/ce-e-nou/";
 	const response = await axios(url);
 	const $ = load(response.data);
 
+  // Get the title of the latest article and remove new lines and spaces
 	latestArticle.title = $("body")
 		.find(
 			"#article > div.whatsNewItems > div.synopsis.sqs.desc.first > div.syn-body.sqs > h3 > a"
@@ -19,6 +22,8 @@ const scrape = async () => {
 		.text()
 		.replace("\n", "")
 		.trim();
+
+  // Get the source of the latest article, remove new lines, the spaces, turn it into lowercase and uppercase the first letter 
 	latestArticle.src = $("body")
 		.find(
 			"#article > div.whatsNewItems > div.synopsis.sqs.desc.first > div.syn-body.sqs > p.contextTitle"
@@ -28,6 +33,8 @@ const scrape = async () => {
 		.trim()
 		.toLowerCase()
 		.replace(/^\w/, (c) => c.toUpperCase());
+
+  // Get the url of the latest article
 	latestArticle.url = `https://jw.org/${$("body")
 		.find(
 			"#article > div.whatsNewItems > div.synopsis.sqs.desc.first > div.syn-body.sqs > h3 > a"
@@ -35,6 +42,7 @@ const scrape = async () => {
 		.attr("href")}`;
 };
 
+// Store the latest article in the database
 const storeLastArticle = async () => {
 	await setDoc(doc(db, "article", "latestArticle"), {
 		title: latestArticle.title,
@@ -43,6 +51,7 @@ const storeLastArticle = async () => {
 	});
 };
 
+// Send an email with the latest article, if it's different from the previous one
 const sendEmail = async () => {
 	const docRef = doc(db, "article", "latestArticle");
 	const docData = await getDoc(docRef);
@@ -75,8 +84,10 @@ const sendEmail = async () => {
 	}
 };
 
+// Run the functions
 const checkForNewArticle = async () => {
   await scrape().then(storeLastArticle).then(sendEmail);
 }
 
+// Check for new articles hourly
 setInterval(checkForNewArticle, 1000 * 60 * 60);
